3303570a494149e6e1cd42234c13a801ac70dee0
John Nduli <yohanaizraeli@gmail.com>
2017-06-25 15:25:02 +0300

    Implement models for comic site
    Added the concept, sketch, comic and comic_images models to the repo.

--- comicsite/comics/models.py
+++ comicsite/comics/models.py
@@ -1,10 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 
-class strip ( models.Model):
-    slug = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
+class Concept ( models.Model ):
+    characters_no = models.IntegerField()
+    comicstrips_no = models.IntegerField()
+    environment = models.TextField()
+    conversation = models.TextField()
+    deleted = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=timezone.now())
+    user = models.ForeignKey(User)
+
+class Sketch ( models.Model ):
+    concept = models.ForeignKey(Concept)
+    image = models.ImageField(upload_to='sketches')
+    user = models.ForeighKey(User)
+    date_created = models.DateTimeField(default=timezone.now())
+    deleted = models.BooleanField(default=False)
+
+class Comic ( models.Model ):
+    user = models.ForeignKey(User)
+    sketch = models.ForeighKey(Sketch)
     description = models.TextField()
-    alternative = models.CharField(max_length=200)
-    
+    title = models.CharField(max_length = 30)
+    slug = models.SlugField(unique=True)
+    published = models.BooleanField(default=True)
+    deleted = models.BooleanField(default=True)
+    date_created = models.DateTimeField(default=timezone.now())
+    date_published = models.DateTimeField(default=timezone.now())
+    work_files = models.FileField(upload_to='work_files')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = "%i-%s" %(
+                    self.id, slugify(self.title)
+                    )
+        super(Comic, self).save(*args, **kwargs)
+
+class Strip ( models.Model ):
+    comic - models.ForeignKey(Comic)
+    image = models.ImageField(upload_to='strips')
+    position = models.IntegerField();

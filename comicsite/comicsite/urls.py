88de4e3f0a3e25142f4323d9e670d67da8bb3734
John Nduli <yohanaizraeli@gmail.com>
2017-09-24 15:21:34 +0300

    Modify urls such that system redirects to home page

--- comicsite/comicsite/urls.py
+++ comicsite/comicsite/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('authenticate.urls', namespace='auth') ),
     url(r'^concept/', include('concept.urls', namespace='concept') ),
-    url(r'^comics/', include('comics.urls',namespace='comics') ),
+    url(r'^system/', include('comics.urls',namespace='comics') ),
     url(r'^sketch/', include('sketch.urls', namespace='sketch') ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

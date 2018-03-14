af2f70eee803904d45b9572285e992b32738c063
John Nduli <yohanaizraeli@gmail.com>
2018-03-14 20:49:22 +0300

    Refactor code to have one overall upload template

--- comicsite/concept/gimp/views.py
+++ comicsite/concept/gimp/views.py
@@ -16,6 +16,7 @@ class GimpCreate(CreateView, SuccessMessageMixin):
     model = Gimp
     fields = ['file_gimp']
     success_message = "Gimp file successfully uploaded"
+    template_name = "comicsite/upload_form.html"
 
     def get_success_url(self):
         return reverse('concept:detail_concept', args=[self.kwargs['pk']])
@@ -24,12 +25,17 @@ class GimpCreate(CreateView, SuccessMessageMixin):
         form.instance.user = self.request.user
         form.instance.concept = Concept.objects.get(pk=self.kwargs['pk'])
         return super(GimpCreate, self).form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super(GimpCreate, self).get_context_data(**kwargs)
+        context['title'] = 'Add Gimp File'
+        return context
 
 @method_decorator(login_required(login_url=reverse_lazy('auth:login')),name='dispatch')
 class GimpUpdate(UpdateView, SuccessMessageMixin):
     model = Gimp
     fields = ['file_gimp']
     success_message = "Gimp file successfully updated"
+    template_name = "comicsite/upload_form.html"
 
     def get_object(self, queryset=None):
         gimp = Concept.objects.get(id=self.kwargs['pk']).gimp
@@ -38,3 +44,7 @@ class GimpUpdate(UpdateView, SuccessMessageMixin):
     def get_success_url(self):
         return reverse('concept:detail_concept', args=[self.kwargs['pk']])
 
+    def get_context_data(self, **kwargs):
+        context = super(GimpUpdate, self).get_context_data(**kwargs)
+        context['title'] = 'Update Gimp File'
+        return context

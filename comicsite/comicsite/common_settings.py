784a440495541aa1f6055f32071eb2d31ba66f5a
John Nduli <yohanaizraeli@gmail.com>
2018-03-20 06:35:12 +0300

    Use python-decouple to store secrets
    Save secrets in .env and create .env.example to show sample usage.
    Change settings to use appropriate config variables.

--- comicsite/comicsite/common_settings.py
+++ comicsite/comicsite/common_settings.py
@@ -1,6 +1,9 @@
 import os
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = config('SECRET_KEY')
 
 # Application definition
 
